a90e26fdece52181ea2366f431edaa9e0d96f18c
Mateusz Chmielewski <chmielek1924@gmail.com>
2023-02-13 19:50:18 +0100

    Installed bootstrap-react, react-router-dom

---
+++ src/MainNavigation/MainNavigation.js
@@ -0,0 +1,11 @@
+// import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+
+// const MainNavigation = () => {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route></Route>
+//       </Routes>
+//     </Router>
+//   );
+// };
